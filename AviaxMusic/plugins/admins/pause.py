d35771a75e8f2ffa791cb9ece4784e8f69d0f23a
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-09-01 21:06:28 +0530

    Update pause.py

--- AviaxMusic/plugins/admins/pause.py
+++ AviaxMusic/plugins/admins/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AviaxMusic import app
-from AviaxMusic.core.call import Aviax
+from AviaxMusic.core.call import Hotty
 from AviaxMusic.utils.database import is_music_playing, music_off
 from AviaxMusic.utils.decorators import AdminRightsCheck
 from AviaxMusic.utils.inline import close_markup
