ede43b9b31f783f2b3eb14c3b20b47381b495be6
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-09-01 21:13:56 +0530

    Update watcher.py

--- AviaxMusic/plugins/misc/watcher.py
+++ AviaxMusic/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AviaxMusic import app
-from AviaxMusic.core.call import Aviax
+from AviaxMusic.core.call import Hotty
 
 welcome = 20
 close = 30
