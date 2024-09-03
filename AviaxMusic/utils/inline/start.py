09326fcda2e457ccf5c0de4fac505c4e0fa750f5
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-09-03 10:49:24 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -31,7 +31,6 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://github.com/CyberPixelPro/AviaxMusic"),
         ],
     ]
     return buttons
