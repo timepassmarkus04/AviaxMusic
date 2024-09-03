f189a82007d56b4c9bcf7a5a463dd6e6c9a25826
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-09-03 21:36:45 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -559,7 +559,7 @@ class Call(PyTgCalls):
                     db[chat_id][0]["mystic"] = run
                     db[chat_id][0]["markup"] = "tg"
                 else:
-                    img = await get_thumb(videoid)
+                    img = await gen_thumb(videoid)
                     button = stream_markup(_, videoid, chat_id)
                     run = await app.send_photo(
                         chat_id=original_chat_id,
